89282b20a267a23b637eba66bce47aead2002a7c
Tianze Ds Qiu <slaaker@126.com>
2024-12-13 23:20:43 +0800

    cumulative till 12.13.1

--- utils/xfyun_asr.py
+++ utils/xfyun_asr.py
@@ -15,8 +15,9 @@ api_get_result = '/getResult'
 
 
 class RequestApi(object):
-    def __init__(self, appid, secret_key, upload_file_path):
+    def __init__(self, appid, secret_key, language, upload_file_path):
         self.appid = appid
+        self.language = language
         self.secret_key = secret_key
         self.upload_file_path = upload_file_path
         self.ts = str(int(time.time()))
@@ -37,7 +38,7 @@ class RequestApi(object):
 
 
     def upload(self):
-        print("上传部分：")
+        print("uploading part:")
         upload_file_path = self.upload_file_path
         file_len = os.path.getsize(upload_file_path)
         file_name = os.path.basename(upload_file_path)
@@ -49,12 +50,14 @@ class RequestApi(object):
         param_dict["fileSize"] = file_len
         param_dict["fileName"] = file_name
         param_dict["duration"] = "200"
-        print("upload参数：", param_dict)
+        param_dict['resultType'] = "transfer,predict"
+        param_dict["language"] = self.language
+        print("upload param:", param_dict)
         data = open(upload_file_path, 'rb').read(file_len)
 
         response = requests.post(url =lfasr_host + api_upload+"?"+urllib.parse.urlencode(param_dict),
                                 headers = {"Content-type":"application/json"},data=data)
-        print("upload_url:",response.request.url)
+        print("upload_url:", response.request.url)
         result = json.loads(response.text)
         print("upload resp:", result)
         return result
@@ -69,23 +72,23 @@ class RequestApi(object):
         param_dict['ts'] = self.ts
         param_dict['orderId'] = orderId
         param_dict['resultType'] = "transfer,predict"
+        param_dict["language"] = self.language
         print("")
-        print("查询部分：")
-        print("get result参数：", param_dict)
+        print("getting part:")
+        print("get result param:", param_dict)
         status = 3
         # 建议使用回调的方式查询结果，查询接口有请求频率限制
         while status == 3:
             response = requests.post(url=lfasr_host + api_get_result + "?" + urllib.parse.urlencode(param_dict),
                                      headers={"Content-type": "application/json"})
-            # print("get_result_url:",response.request.url)
             result = json.loads(response.text)
             print(result)
             status = result['content']['orderInfo']['status']
-            print("status=",status)
+            print("status=", status)
             if status == 4:
                 break
             time.sleep(5)
-        print("get_result resp:",result)
+        print("get_result resp:", result)
         return result
 
 
